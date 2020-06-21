dba010db6ecfd1258a4bc7229af0f5e0bf9fc35c
i47med01 <67040497+i47med01@users.noreply.github.com>
2020-06-21 12:55:30 +0300

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author GAMER
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NewJFrame1 f= new NewJFrame1();
+        f.setTitle("Calculator");
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
+        
+}
+}
